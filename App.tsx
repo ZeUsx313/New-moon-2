41efaa5067e3d5288464b9bbdc55335ada3d059f
ZEUS 2 <cobrapk8ggg@gmail.com>
2026-03-22 11:35:03 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './src/screens/Home';
 import NovelPage from './src/screens/NovelPage';
+import Library from './src/screens/Library';
+import MyPage from './src/screens/MyPage';
 
 export default function App() {
   return (
@@ -9,8 +11,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/novel/:slug" element={<NovelPage />} />
-        {/* يمكن إضافة مسار للفصل الفردي إذا رغبت */}
-        {/* <Route path="/novel/:slug/chapter-:chapter" element={<ChapterPage />} /> */}
+        <Route path="/library" element={<Library />} />
+        <Route path="/my-page" element={<MyPage />} />
       </Routes>
     </Router>
   );
